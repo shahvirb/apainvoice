446c7e79dcac2e284fcf86ec49d3f529a6aaf4c4
Shahvir Buhariwalla <shahvirb@gmail.com>
2024-08-19 12:51:24 -0500

    use the DBPATH env var if set to find the db file

--- apainvoice/db.py
+++ apainvoice/db.py
@@ -1,13 +1,22 @@
 import sqlmodel
+import os
+import logging
+
+logger = logging.getLogger(__name__)
 
 
-def create_engine(dbname: str = "sqlite:///apainvoice.db"):
+def get_db_path() -> str:
+    path = os.getenv("DBPATH", "apainvoice.db")
+    return f"sqlite:///{path}"
+
+
+def create_engine(dbname: str = ""):
+    if not dbname:
+        dbname = get_db_path()
     engine = sqlmodel.create_engine(dbname)
     sqlmodel.SQLModel.metadata.create_all(engine)
     return engine
 
 
 if __name__ == "__main__":
-    engine = create_engine()
-    with sqlmodel.Session(engine) as session:
-        pass
+    pass
