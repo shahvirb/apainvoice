c52ec845d1877792768d6e5f7620f3fe6d6fa150
shahvirb <shahvirb@gmail.com>
2025-01-23 19:58:57 -0600

    updating for latest team

--- apainvoice/players.py
+++ apainvoice/players.py
@@ -3,14 +3,14 @@ import base64
 
 
 B64_PLAYERS = [
-    "TWFkaGF2IFNoYXJtYQ==",  # Ma Sh
     "U3RlcGhhbmllIFBhcGFnZW9yZ2U=",  # St Pa
-    "U2Fta2l0IFNoYWg=",  # Sa Sh
     "RGlvbiBLaW5n",  # Di Ki
     "QWxiZXJ0byBDYWxkZXJvbg==",  # Al Ca
     "Sm9lIEZsb3Jlcw==",  # Jo Fl
     "U2hhaHZpciBCdWhhcml3YWxsYQ==",  # Sh Bu
     "UGF1bCBTaGVsdG9u",  # Pa Sh
+    "TWl0Y2hlbGwgVGlqZXJpbmE=" , # Mi Ti
+    "QW5naWUgUGF5bmU=",  # An Pa
 ]
 
 
